9903bcff6470c6c2e14c0581f0d7ddb7324cf750
Kaio Pomini <kaiopomini@gmail.com>
2021-05-19 21:18:08 -0300

    first commit

---
+++ starter/node-api/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// app start
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+// database
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi', 
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  }
+);
+
+requireDir('./src/models');
+
+
+// routes
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
